5995b3a428790fc388c7c4e6d6f01fa509e4058b
VittoLauria <vittoriolauria10@gmail.com>
2025-05-15 18:05:45 +0200

    calcolatrice

---
+++ assignament/compito1/Program.cs
@@ -0,0 +1,69 @@
+﻿// calcolatrice
+
+
+
+//chiedo all'utente il primo numero 
+
+
+Console.WriteLine("dimmi il primo numero");
+int numero1 = int.Parse (Console.ReadLine());
+
+//chiedo all'utente di inserire il secondo numero 
+
+Console.WriteLine("dimmi il secondo numero");
+int numero2 = int.Parse (Console.ReadLine());
+
+//chiedo all'utente l'operazione da eseguire
+
+Console.WriteLine("dimmi che operazione vuoi fare +, -, /, *");
+
+string operatore = Console.ReadLine ();
+
+
+// ora lavoriamo con le variabili acquisite
+
+
+ switch (operatore) 
+ {
+case "+": 
+    int addizione = numero1 + numero2;
+    Console.WriteLine($"{addizione}");
+    break;
+case "-": 
+    int sottrazione = numero1 - numero2;
+    Console.WriteLine($"{sottrazione}");
+    break;
+
+
+case "/": 
+
+if ( numero2 == 0)
+{ 
+    Console.WriteLine("Non puoi dividere per 0 ERRORE");
+}
+else 
+{
+     int divisione = numero1 / numero2;
+    Console.WriteLine($"{divisione}");
+}
+break;
+
+case "*": 
+    int moltiplicazione = numero1 * numero2;
+    Console.WriteLine($"{moltiplicazione}");
+    break;
+
+      default:
+       Console.WriteLine("non hai inserito l'operatore valido scegli tra: +, -, /, * ");
+       break;  
+ }
+
+
+//se l'operazione scelta dall'utente è es.divisione
+//se il secondo numero è zero 
+
+//allora stampo un messaggio di errore
+
+//altrimenti eseguo l'operazione
+
+//stampo il risultato
---
+++ assignament/compito1/README.md
@@ -0,0 +1,28 @@
+# Calcolatrice
+## Descirzione
+
+Scrivere un programma che simuli una calcolatrice.Il programma deve essere in grado di eseguire le seguenti operazioni
+- somma
+- sottrazione
+- moltiplicazione
+- divisione
+
+## Requisiti
+
+Il programma deve chiedere all'utente di inserire due numeri e l'operazione da eseguire.Il programma deve quindi eseguire l'operazione e stampare il risultato
+Il programma deve gestire la divsione per zero e stampare un messaggio di errore in caso per divisione per zero.
+
+## Suggerimenti
+
+- Il programma chiede all'utente una cosa per volta tipo prima chiede il primo numero poi il secondo numero poi l'operazione da eseguire
+- In modo da gestire correttamente l'errore di divisione per zero,
+ > in pratica devo usare un if per controllare se il secondo numero è zero prima di eseguire l'operazione di divisione
+- Il programma deve essere in grado di gestire solo numeri interi senza numeri decimali
+- Il programma deve essere in grado di gestire solo le operazioni di somma, sottrazione, divisione e moltiplicazione.Se l'utente inserisce un'operazione non valida, il programma deve stampare un messaggio di errore
+- Posso creare l'archetipo dotney con il comando `dotnet new console` e poi copiare il codice in questo file
+
+## Codice
+
+```csharp
+
+```
