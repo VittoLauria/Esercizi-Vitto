f97bfb3f650ea96cea61b60d1fef6e49130fb243
VittoLauria <vittoriolauria10@gmail.com>
2025-09-09 11:09:31 +0200

    fixato user

--- 22-Fullstack-backend/FrontEnd/MyMiniApp/src/app/user-form/user-form.ts
+++ 22-Fullstack-backend/FrontEnd/MyMiniApp/src/app/user-form/user-form.ts
@@ -4,14 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { User } from '../models/user.model';
 import { UserService } from '../services/user';
-import { constructorChecks } from '@angular/cdk/schematics';
 
 @Component({
   selector: 'app-user-form',
-  standalone: true;
+  standalone: true,
   imports: [CommonModule, FormsModule, RouterModule],
   template: `
-  <h2>{{isNew ? 'Aggiungi' : 'Modifica'}} utente</h2>
+    <h2>{{isNew ? 'Aggiungi' : 'Modifica'}} utente</h2>
     <form (ngSubmit)="save()">
       <label>Nome: <input [(ngModel)]="model.name" name="name" required></label><br>
       <label>Città: <input [(ngModel)]="model.address.citta" name="addressCitta" required></label><br>
@@ -20,13 +19,13 @@ import { constructorChecks } from '@angular/cdk/schematics';
       <button type="submit">Salva</button>
       <button type="button" (click)="goBack()">Annulla</button>
     </form>
-    `
+  `
 })
-export class UserForm Implements OnInit {
+export class UserForm implements OnInit {
   model: User = {
-    id: 0,
-    name: "",
-    address: { citta: '', via: '', cap: '' }
+  id: 0,
+  name: '',
+  address: { citta: '', via: '', cap: '' }
   };
   isNew = true;
 
@@ -34,15 +33,16 @@ export class UserForm Implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
     private service: UserService
-  ) { }
+  ) {}
 
   ngOnInit() {
-    const id = this.route.snapshot.paraMap.get('id');
+    const id = this.route.snapshot.paramMap.get('id');
     if (id) {
       this.isNew = false;
       this.service.get(+id).subscribe(u => this.model = u);
     }
   }
+
   save() {
     if (this.isNew) {
       this.service.add(this.model).subscribe(() => this.goBack());
@@ -52,6 +52,6 @@ export class UserForm Implements OnInit {
   }
 
   goBack() {
-    this.router.navigate(['/user']);
+    this.router.navigate(['/users']);
   }
 }
